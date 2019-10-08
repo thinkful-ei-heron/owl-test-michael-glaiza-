651309642e61a88187abe01a23fb284a5eb0d6d7
Glaiza Wagner <glaizawagner@gmail.com>
2019-10-08 11:42:25 -0700

    temporary

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react'
+import Participants from './Participants'
 
-function App() {
-  return (
-    <main className='App'>
-      {/* content goes here */}
-    </main>
-  );
+export class App extends Component {
+  
+  render() {
+    console.log('for app')
+    return (
+      <div className="App">
+        
+
+        <Participants />
+
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
---
+++ src/Participants.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+
+export class Participants extends Component {
+
+
+    render() {
+        return this.props.Participants.map((participantList) => (
+            
+        ));
+    }
+}
+
+export default Participants
+
